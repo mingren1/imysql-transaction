2cfc6b391b734f2439c084ac6bb01c64912e0faa
978758375@qq.com <wenhao978758375>
2019-03-18 23:39:08 +0800

    Initial commit

---
+++ src/main/java/com/athome/mysql/imysqltransaction/ImysqlTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.athome.mysql.imysqltransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImysqlTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImysqlTransactionApplication.class, args);
+	}
+
+}
